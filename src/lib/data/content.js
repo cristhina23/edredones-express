5781a7ac6f381c5fb85c0f4efd2fc059cc8f7769
carl3per <carl3per@gmail.com>
2026-06-09 13:30:50 -0500

    Update content.js

--- src/lib/data/content.js
+++ src/lib/data/content.js
@@ -50,7 +50,7 @@ export const content = {
       {
         title: "Edredones de Plumas",
         subtitle: " S/45",
-        description: "Queen - King",
+        description:     "Queen - King",
       }
       
     ],
